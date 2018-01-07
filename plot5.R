b5e219b20f2b684191935df58f839e260adc6d34
Jose Pi <31528986+josempi@users.noreply.github.com>
2018-01-06 20:57:40 -0800

    Add files via upload

--- plot5.R
+++ plot5.R
@@ -10,7 +10,7 @@
 ## File source: EPA National Emissions Inventory                                                    ##
 ## Link: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip                         ##
 ## Environment: RStudio 1.0.153, R version 3.4.3                                                    ##
-## Question: How have emissions from motor vehicles sources chnaged from 1999-2008 in Baltimore     ##
+## Question: How have emissions from motor vehicles sources changed from 1999-2008 in Baltimore     ##
 ##           City?                                                                                  ##
 ##                                                                                                  ##
 ######################################################################################################
@@ -91,7 +91,7 @@ ggplot(data=NEIsummary, aes(x=NEIsummary$`NEIBaltimore$year`, y=NEIsummary$SPM2.
     geom_line(size=1.5) +
     geom_point(size=3) +
     xlab("Years") + ylab("Total PM2.5 Emissions") +
-    ggtitle("City of Baltimore, Maryland, PM2.5 Total Vehicle Emissions")
+    ggtitle("Baltimore City, Maryland, PM2.5 Total Vehicle Emissions")
 
 ## Print the plot to a file
 png(filename = "plot5.png", width = 480, height = 480)
@@ -99,5 +99,5 @@ ggplot(data=NEIsummary, aes(x=NEIsummary$`NEIBaltimore$year`, y=NEIsummary$SPM2.
     geom_line(size=1.5) +
     geom_point(size=3) +
     xlab("Years") + ylab("Total PM2.5 Emissions") +
-    ggtitle("City of Baltimore, Maryland, PM2.5 Total Vehicle Emissions")
+    ggtitle("Baltimore City, Maryland, PM2.5 Total Vehicle Emissions")
 dev.off()
